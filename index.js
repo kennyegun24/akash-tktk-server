7c6b78fc458370c97844b321f1163627c436fc34
Kennyegun24 <kennyegun240@gmail.com>
2024-03-01 12:59:29 +0100

    debug2

--- index.js
+++ index.js
@@ -93,6 +93,7 @@ app.post("/initiate/video/upload", upload.single("video"), async (req, res) => {
   const fileSize = req.body.fileSize;
   const videoBuffer = req.file ? req.file.buffer : null;
   console.log(req.file);
+  console.log(req.body);
   try {
     const initializeUploadToTiktokApi = async () => {
       try {
